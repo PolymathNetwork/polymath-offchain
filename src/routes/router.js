dfef81138d6e44086bfe4f932ec0850e7b31ae27
Boris Shevchenko <bshevchenkoweb@gmail.com>
2018-08-20 12:07:04 +0300

    notice

--- src/routes/router.js
+++ src/routes/router.js
@@ -5,11 +5,13 @@ import Router from 'koa-router'
 import auth from './auth'
 import email from './email'
 import providers from './providers'
+import notice from './notice'
 
 const router = new Router()
 
 router.use(auth.routes())
 router.use(email.routes())
 router.use(providers.routes())
+router.use(notice.routes())
 
 export default router
