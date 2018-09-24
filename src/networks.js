50c44ab1cd39733179a8b1fc49aee6666279c630
Rafael Vidaurre <narzerus@gmail.com>
2018-09-24 20:33:42 -0300

    fix: use env vars for node URLs

--- src/networks.js
+++ src/networks.js
@@ -2,7 +2,6 @@ export const NETWORK_MAIN = '1'
 export const NETWORK_KOVAN = '42'
 
 export default {
-  [NETWORK_MAIN]: 'https://mainnet.infura.io/C1yXP10CIm9kzJyTfUMO',
-  [NETWORK_KOVAN]:
-    'https://heartily-internal-escargot.quiknode.io/46df7525-5518-428e-b30b-9dea09480213/4oM662IRx_2tZJq3ht4wdQ==/',
+  [NETWORK_MAIN]: process.env.NETWORK_MAIN_URL,
+  [NETWORK_KOVAN]: process.env.NETWORK_KOVAN_URL
 }
